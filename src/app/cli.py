efcde250af885b01f12e01438c2a1c256d4f3c02
Gerardo De Luisi <deluisigerardo@gmail.com>
2025-01-28 18:29:43 +0100

    Created RAD

--- src/app/cli.py
+++ src/app/cli.py
@@ -5,11 +5,14 @@ from git import Repo,InvalidGitRepositoryError
 from pathlib import Path
 from logging import error,info
 from typing import Optional,Sequence
+
 def main(args:Optional[Sequence[str]]=None):
     parser=ArgumentParser(prog="project-viewer")
-    parser.add_argument('dir',nargs='?', default=Path.cwd().as_posix(), type=str) 
+    parser.add_argument('dir',nargs='?', default=Path.cwd().as_posix(), type=str)
     namespace=parser.parse_args(args)
     dir=str(namespace.dir)
+    if dir==".":
+        dir=Path.cwd().as_posix()
     info(f"Opening git repository at {dir}")
     if not Path(dir).is_dir():
         error(f"Path {dir} is not a directory")
