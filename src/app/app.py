0a46ab527af492c76e8b90a89d5284d989e17aff
Gerardo De Luisi <deluisigerardo@gmail.com>
2025-03-04 10:26:03 +0100

    major interface revamp

--- src/app/app.py
+++ src/app/app.py
@@ -1,5 +1,5 @@
 from webbrowser import open
-from dash import Dash,html,dcc,page_container,Input,Output,callback,no_update,DiskcacheManager
+from dash import Dash,html,dcc,page_container,Input,Output,callback,no_update,DiskcacheManager,State,set_props
 from typing import Union,Optional
 from datetime import date
 from pathlib import Path
@@ -9,34 +9,52 @@ from time import strptime,strftime
 import pandas as pd
 import numpy as np
 import plotly.express as px
-from src._internal import make_commit_dataframe,make_author_dataframe
 from src.utility.logs import setup_logging
 import dash_bootstrap_components as dbc
 def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
     path=repo_path if isinstance(repo_path,str) else repo_path.as_posix()
     # print(path)
-    app=Dash(name="Project Visualization Tool",title="PVT",assets_folder=Path(__file__).parent.parent.joinpath("gui","assets").as_posix(),external_stylesheets=[dbc.themes.BOOTSTRAP],use_pages=True,pages_folder=Path(__file__).parent.parent.joinpath("gui","pages").as_posix())
+    app=Dash(name="Project Visualization Tool",title="PVT",assets_folder=Path(__file__).parent.parent.joinpath("gui","assets").as_posix(),external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP],use_pages=True,pages_folder=Path(__file__).parent.parent.joinpath("gui","pages").as_posix())
     navbar = dbc.NavbarSimple(
         children=[
             dbc.NavItem(dbc.NavLink("Home", href="/")),
             dbc.NavItem(dbc.NavLink("Directory analysis", href="/dir")),
+            dbc.Button(id="reload_button",children=[html.I(className="bi bi-arrow-counterclockwise p-1")],className="p1 bg-transparent border-0"),
         ],
         brand="Project Visualization Tool",
         brand_href="/",
         color="dark",
         dark=True,
         className="mb-2",
+        sticky=True,
+        fluid=True
+    )
+    sidebar_stack=dbc.Stack(
+        [
+            html.Div([dbc.Label(["Branch Picker"]),dcc.Dropdown(id="branch_picker",searchable=True,clearable=True,placeholder="Branch name")]),
+            html.Div([dbc.Label(["Author Picker"]),dcc.Dropdown(id="author_picker",searchable=True,clearable=True,placeholder="Author name")]),
+        ], gap=2,className="p-2"
     )
     app.layout = html.Div([
         navbar,
         dbc.Container([
-            dcc.Store(id="commit_df"),
-            dcc.Store(id="author_df"),
+            # dcc.Store(id="commit_df"),
+            # dcc.Store(id="author_df"),
+
+            dcc.Store(id="branch_cache"),
             dcc.Store("repo_path",data=path,storage_type="session"),
-            dbc.Row([dbc.Col(
-                        children=[dbc.Label(["Branch Picker"]),dcc.Dropdown(id="branch_picker",searchable=True,clearable=True,placeholder="Branch name")],
-                        width=5),],align="center"),
-            page_container
+            dcc.Loading(fullscreen=True,children=[
+                dcc.Store(id="commit_df_cache",storage_type="session"),
+                ]),
+            dbc.Row([
+                dbc.Col(
+                        children=[sidebar_stack],
+                        width=2,align="start"), 
+                dbc.Col(
+                        children=[page_container],
+                        width=10,align="end"), 
+            ],align="start"),
+            
         ],fluid=True)
     ])
     if not cicd_test:
@@ -45,3 +63,61 @@ def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
         else:
             open("http://localhost:8050/")
             serve(app.server,host="localhost",port=8050,_quiet=True)
+
+@callback(
+        Output("commit_df_cache","data"),
+        Input("reload_button","n_clicks"),
+        State("repo_path","data"),
+        State("commit_df_cache","data"),
+        prevent_initial_call=True
+)
+def listen_data(_,data,cache):
+        rp=RepoMiner(data)
+        set_props("branch_picker",{"options":list(( b.name for b in rp.get_branches(deep=False)))})
+        set_props("author_loader",{"display":"show"})
+        set_props("author_loader_graph",{"display":"show"})
+        m_count=None
+        commit_df=pd.DataFrame()
+        for commit_list in rp.lazy_load_commits(max_count=m_count):
+                cl_df=pd.concat(map(lambda c:c.get_dataframe(),commit_list))
+                # print(cl_df.info())
+                commit_df=pd.concat([commit_df,cl_df])
+        commit_df.reset_index(inplace=True)
+        commit_df["date"]=pd.to_datetime(commit_df["date"])
+        commit_df["dow"]=commit_df["date"].dt.day_name()
+        commit_df["dow_n"]=commit_df["date"].dt.day_of_week
+        authors=commit_df["author_name"].unique().tolist()
+        set_props("author_picker",{"options":authors})
+        set_props("author_loader_graph",{"display":"auto"})
+        set_props("author_loader",{"display":"auto"})
+        return commit_df.to_dict("records")
+    
+@callback(
+        Output("branch_cache","data"),
+        Input("branch_picker","value"),
+        Input("author_picker","value"),
+        Input("commit_df_cache","data"),
+        State("repo_path","data"),
+        prevent_initial_call=True
+)
+def filter_branch_data(v,author,cache,path):
+        branch=None if not v or "all" == v else v            
+        if v or author and v!="all":
+            rp=RepoMiner(path)
+            df=pd.DataFrame(cache)
+            b=rp.get_branch(branch)
+            # print(len(b.commits))
+            df=df[df["commit_hash"].isin(b.commits)] if v else df
+            df=df[df["author_name"]==author] if author else df
+            # df.info()
+            return df.to_dict("records")
+        return cache
+    
+@callback(
+        Output("author_cache","data"),
+        Input("commit_df_cache","data"),
+        prevent_initial_call=True
+)
+def filter_branch_data(cache):
+        
+        return no_update
--- src/gui/pages/homepage.py
+++ src/gui/pages/homepage.py
@@ -17,14 +17,7 @@ dash.register_page(__name__,"/")
 common_labels={"date":"Date","commit_count":"Number of commits","author_email":"Author's email","author_name":"Author's name","dow":"Day of the week"}
 
 layout = dbc.Container([
-        dcc.Store(id="branch_cache",storage_type="session"),
-        dcc.Loading(fullscreen=True,children=[
-                dcc.Store(id="commit_df_cache",storage_type="session"),
-                ]),
         dbc.Row(id="choices",children=[
-                dbc.Col(
-                        children=[dbc.Button(id="reload_button",children="Refresh Data")],
-                        width=1),
                 dbc.Col(
                         children=[dbc.Label(["Display mode picker"]),dcc.Dropdown(id="x_picker",value="dow",options=[{"label":"Day of week","value":"dow"},{"label":"Per date","value":"date"}]),],
                         width=5),
@@ -50,75 +43,26 @@ layout = dbc.Container([
         # html.Div(id="test-div")
 ])
 
-# @callback(
-#         Output("br","children"),
-#         Input("graph","clickData"),
-#         Input("graph","relayoutData"),
-#         Input("graph","hoverData"),
-#         Input("graph","selectedData") 
-# )
-# def test(click,rel,ho,sel):
-#         return json.dumps(dict(click=click,rel=rel,hover=ho,sdata=sel))
 @callback(
         Output("graph","figure"),
         Input("x_picker","value"),
         Input("branch_cache","data"),
+        Input("author_picker","value"),
         State("branch_picker","value"),
-        prevent_initial_call=True
 )
-def update_count_graph(pick,data,branch):
+def update_count_graph(pick,data,author,branch):
         commit_df=pd.DataFrame(data)
         if pick =="dow":
-                count_df=commit_df.groupby(["dow","dow_n"]).size().reset_index(name="commit_count")
+                count_df=commit_df.groupby(["dow","dow_n"])
+                count_df=count_df.size().reset_index(name="commit_count")
                 count_df.sort_values("dow_n",ascending=True,inplace=True)
                 fig=px.bar(count_df,x=pick,y="commit_count",labels=common_labels,title=f"Commit Distribution {branch if branch else ''}")
         else:
                 count_df=commit_df.groupby(["date"]).size().reset_index(name="commit_count")
                 fig=px.area(count_df,hover_data=["date"],x=pick,y="commit_count",labels=common_labels,title=f"Commit Distribution {branch if branch else ''}")        
         return fig
-@callback(
-        Output("commit_df_cache","data"),
-        Input("reload_button","n_clicks"),
-        State("repo_path","data"),
-        State("commit_df_cache","data"),
-)
-def listen_data(_,data,cache):
-        rp=RepoMiner(data)
-        set_props("branch_picker",{"options":list(( b.name for b in rp.get_branches(deep=False)))})
-        set_props("author_loader",{"display":"show"})
-        set_props("author_loader_graph",{"display":"show"})
-        m_count=None
-        commit_df=pd.DataFrame()
-        for commit_list in rp.lazy_load_commits(max_count=m_count):
-                cl_df=pd.concat(map(lambda c:c.get_dataframe(),commit_list))
-                # print(cl_df.info())
-                commit_df=pd.concat([commit_df,cl_df])
-        commit_df.reset_index(inplace=True)
-        commit_df["date"]=pd.to_datetime(commit_df["date"])
-        commit_df["dow"]=commit_df["date"].dt.day_name()
-        commit_df["dow_n"]=commit_df["date"].dt.day_of_week
-        
-        set_props("author_loader_graph",{"display":"auto"})
-        set_props("author_loader",{"display":"auto"})
-        return commit_df.to_dict("records")
-@callback(
-        Output("branch_cache","data"),
-        Input("branch_picker","value"),
-        Input("commit_df_cache","data"),
-        State("repo_path","data"),
-        prevent_initial_call=True
-)
-def filter_branch_data(v,cache,path):
-        branch=None if not v or "all" == v else v
-        if v and v!="all":
-                rp=RepoMiner(path)
-                df=pd.DataFrame(cache)
-                b=rp.get_branch(branch)
-                # print(len(b.commits))
-                df=df[df["commit_hash"].isin(b.commits)]
-                # df.info()
-                return df.to_dict("records")
-        return cache
+
+
 @callback(
         Output("author_graph","figure"),
         Output("author_loader","display"),
@@ -128,7 +72,7 @@ def filter_branch_data(v,cache,path):
         Output("date_slider","marks"),
         Input("branch_cache","data"),
         Input("date_slider","value"),
-        prevent_initial_call=True
+
 )
 def populate_author_graph(data,value):
         df=pd.DataFrame(data)
