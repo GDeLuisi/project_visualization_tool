b4b6841e413ed13d72750296eb152b4110ec7524
GeggeDL <102797969+GDeLuisi@users.noreply.github.com>
2025-06-20 08:39:55 +0200

    End of Development cycle 0.3.0 (#94)
    * fixed #80
    
    * Fixed #84 , revamped author effort overtime #81
    
    * Updated documentation #83
    
    * End 0.3 development cycle (#91)
    
    * Major callbacks refactor, started commit retrevial optimization #86
    
    * Git module substituion with repominer package
    
    * REstructured test cases for latest major changes
    
    * Major change in table usage for commits and authors discovery, changed library used for repository mining
    
    * Added modal for commit
    
    * Fixed treemap generation for directory analysis
    
    * Major overhaul on information loading order and optimization, minor changes across the whole project
    
    * Fixed test truck facotr calculation test case
    
    * Trying possible fix for OSx related errors
    
    * Minor optimization and correction patch
    
    * Minor fix to test
    
    * Minor graphic enhancement #93

--- src/_internal/__init__.py
+++ src/_internal/__init__.py
@@ -1,4 +1,3 @@
-from .git_mining import *
 from .data_typing import *
 from .file_parser import *
 from .data_preprocessing import *
--- src/app/app.py
+++ src/app/app.py
@@ -1,14 +1,17 @@
 from webbrowser import open
-from dash import Dash,html,dcc,page_container,Input,Output,callback,no_update,DiskcacheManager,State,set_props
+from dash import Dash,html,dcc,page_container,Input,Output,callback,no_update,State,set_props
 from typing import Union,Optional
 from datetime import date
 from pathlib import Path
-from waitress import serve,server
-from concurrent.futures import ThreadPoolExecutor,ProcessPoolExecutor
-from src._internal import RepoMiner
+from waitress import serve
+from concurrent.futures import ProcessPoolExecutor,ThreadPoolExecutor
+from repository_miner import RepoMiner
+from src.utility import get_dataframe
+from src.app.helper import retrieve_contribution_data,parallel_commit_retrievial
 import pandas as pd
 import dash_bootstrap_components as dbc
 import re
+from functools import partial
 from logging import getLogger
 logger=getLogger()
 
@@ -17,7 +20,7 @@ def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
     path=repo_path if isinstance(repo_path,str) else repo_path.as_posix()
     # print(path)
     pr_name=re.subn(r"_|-"," ",Path(path).name)[0].capitalize()
-    app=Dash(name=pr_name,title="PVT",assets_folder=assets_folder.as_posix(),external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP,assets_folder.joinpath("css").as_posix()],use_pages=True,pages_folder=Path(__file__).parent.parent.joinpath("gui","pages").as_posix())
+    app=Dash(name=__name__,title="PVT",assets_folder=assets_folder.as_posix(),external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP,assets_folder.joinpath("css").as_posix()],use_pages=True,pages_folder=Path(__file__).parent.parent.joinpath("gui","pages").as_posix())
     navbar = dbc.NavbarSimple(
         children=[
             dbc.NavItem(dbc.NavLink("Home", href="/")),
@@ -25,7 +28,7 @@ def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
             dbc.Button(id="reload_button",children=[html.I(className="bi bi-arrow-counterclockwise p-1")],className="p1 bg-transparent border-0",value=0),
             dbc.Button(id="open_info",children=[html.I(className="bi bi-list p-1")],className="p1 bg-transparent border-0"),
         ],
-        brand="Project Visualization Tool",
+        brand=pr_name,
         brand_href="/",
         color="dark",
         dark=True,
@@ -33,7 +36,9 @@ def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
         sticky=True,
         fluid=True
     )
-    
+    logger.info("Loading necessary info, hang tight")
+    contributions,tr_fa=retrieve_contribution_data(path)
+    logger.info("You're good to go")
     general_options=dbc.Offcanvas(id="sidebar_info",title="Directory discovery",is_open=False,children=
         [dbc.Stack(
         [
@@ -41,92 +46,91 @@ def start_app(repo_path:Union[str|Path],cicd_test:bool,env:bool):
         ], gap=2,className="p-2")
         ])
     app.layout = html.Div([
-        dcc.Store(id="contribution_cache"),
-        dcc.Store(id="truck_cache"),
-        dcc.Store(id="branch_cache"),
+        dcc.Store(id="contribution_cache",data=contributions.to_dict("records"),storage_type="session"),
+        dcc.Store(id="truck_cache",data=tr_fa,storage_type="session"),
+        dcc.Loading(children=[
+            dcc.Store(id="branch_cache"),
+            dcc.Store(id="commit_df_cache",storage_type="memory"),
+            ],fullscreen=True),
         dcc.Store(id="authors_cache"),
         dcc.Store("repo_path",data=path,storage_type="session"),
         navbar,
         general_options,
-        
         dbc.Row([ 
                 dbc.Col(
                         children=[
                             dbc.Container([
-                            dcc.Loading(fullscreen=True,children=[
-                                dcc.Store(id="commit_df_cache",storage_type="memory"),
-                                ]),
                             ],fluid=True),
                             page_container
                             ],
                         width=12,align="end"), 
             ],align="start")        
     ])
-    logger.info("Loading necessary info, hang tight")
+    
     if not cicd_test:
         if env=="DEV":
             app.run(debug=True,dev_tools_hot_reload=True)
         else:
-            open("http://localhost:8050/")
-            serve(app.server,host="localhost",port=8050,_quiet=True,threads=100)
+            open("http://localhost:80/")
+            serve(app.server,host="localhost",port=80,_quiet=True,threads=100)
 
 @callback(
         Output("commit_df_cache","data"),
-        Output("truck_cache","data"),
-        Output("contribution_cache","data"),
-        Output("authors_cache","data"),
         Input("reload_button","n_clicks"),
-        State("repo_path","data"),
-        State("commit_df_cache","data"),
+        State("repo_path","data")
 )
-def listen_data(_,data,cache):
-        if cache and _==0:
-            return no_update,no_update,no_update,no_update
+def listen_data(_,data):
         rp=RepoMiner(data)
-        with ProcessPoolExecutor() as executor:
-            result=executor.submit(rp.get_truck_factor)
-        set_props("branch_picker",{"options":list(( b.name for b in rp.get_branches(deep=False)))})
-        # set_props("author_loader",{"display":"show"})
-        # set_props("author_loader_graph",{"display":"show"})
-        m_count=None
-        commit_df=pd.DataFrame()
-        authors=pd.DataFrame()
-        for commit_list in rp.lazy_load_commits(max_count=m_count):
-                cl_df=pd.concat(map(lambda c:c.get_dataframe(),commit_list))
-                # print(cl_df.info())
-                commit_df=pd.concat([commit_df,cl_df])
-        commit_df.reset_index(inplace=True)
+        set_props("branch_picker",{"options":list(( b.name for b in rp.local_branches()))})
+        commits=parallel_commit_retrievial(rp)
+        commit_df=pd.DataFrame(map(lambda c:c.__dict__,commits))
+        # commit_df.reset_index(inplace=True,drop=True)
         commit_df["date"]=pd.to_datetime(commit_df["date"])
         commit_df["dow"]=commit_df["date"].dt.day_name()
         commit_df["dow_n"]=commit_df["date"].dt.day_of_week
-        for author in rp.get_authors():
-            authors=pd.concat([authors,author.get_dataframe()])
-        tr_fa,contributions=result.result()
-        contributions=dict([(f"{a.name}|{a.email}",c) for a,c in contributions.items()])
-        return commit_df.to_dict("records"),tr_fa,contributions,authors.to_dict("records")
+        commit_df=commit_df.drop("tree_func",axis=1)
+        return commit_df.to_dict("records")
+        
+@callback(
+    Output("authors_cache","data"),
+    Input("reload_button","n_clicks"),
+    State("repo_path","data"),
+)
+def load_authors(_,data):
+    rp=RepoMiner(data)
+    authors=pd.DataFrame()
+    for author in rp.authors():
+        authors=pd.concat([authors,get_dataframe(author)])
+    return authors.to_dict("records")
+
+# @callback(
+#     Output("truck_cache","data"),
+#     Output("contribution_cache","data"),
+#     Input("reload_button","n_clicks"),
+#     State("repo_path","data"),
+#     prevent_inital_call=True
+# )
+# def load_truck_info(_,data):
+#     if _==0:
+#         return no_update
+#     contributions=tf_calculator.compute_DOA(tf_calculator.create_contribution_dataframe(data,only_of_files=False))
+#     tf_contributions=tf_calculator.filter_files_of_interest(contributions)
+#     tr_fa=tf_calculator.compute_truck_factor_from_contributions(tf_contributions)
+#     return tr_fa,contributions.to_dict("records")
 
 @callback(
         Output("branch_cache","data"),
         Input("branch_picker","value"),
         Input("commit_df_cache","data"),
         State("repo_path","data"),
-        prevent_initial_call=True
 )
 def filter_branch_data(v,cache,path):
         branch=None if not v or "all" == v else v            
         if v  and v!="all":
             df=pd.DataFrame(cache)
             rp=RepoMiner(path)
-            b=rp.get_branch(branch)
+            b=rp.retrieve_commits(branch)
             df=df[df["commit_hash"].isin(b.commits)] if v else df
             return df.to_dict("records")
-        return cache
-
+        return cache if cache else no_update
     
-# @callback(
-#     Output("test-div","children"),
-#     Input("truck_cache","data"),
-#     Input("contribution_cache","data"),
-# )
-# def test(tf,cont):
-#     return [tf,html.Div([json.dumps(cont)])]
