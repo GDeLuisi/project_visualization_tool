db29ed9d51d75e27b538363ad823768cda280cd8
GDeLuisi <deluisigerardo@gmail.com>
2026-03-16 10:30:41 +0100

    Minor fix

--- src/gui/pages/homepage.py
+++ src/gui/pages/homepage.py
@@ -185,7 +185,11 @@ def populate_generale_info(authors,contributions,branch,path,):
         num_authors=len(authors)
         current_commit=rp.get_commit(branch if branch else current_head)
         local_branches_num=len(rp.local_branches_list())
-        tag_num=len(list(rp.get_tags()))
+        tag_num = 0
+        try:
+            tag_num=len(list(rp.get_tags()))
+        except Exception:
+            tag_num = 0
         texts={
                 "Total number of commits":f"{num_commits}",
                 "Total number of authors":f"{num_authors}",
